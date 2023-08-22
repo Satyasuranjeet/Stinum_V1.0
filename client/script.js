d6eef00f58534417a90cdfefe3198b35c88cfe4b
Satya Suranjeet Jena <102950648+Satyasuranjeet@users.noreply.github.com>
2023-08-22 13:58:22 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://stinum.onrender.com', {
+    const response = await fetch('https://stinum.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
